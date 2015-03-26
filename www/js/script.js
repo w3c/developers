a4e678200abc068355fd3b233773a4c76c6d1ab1
guibbs <guillaume.baudusseau@gmail.com>
2015-03-26 10:27:49 -0400

    add fixed navbar when scrolling

--- www/js/script.js
+++ www/js/script.js
@@ -1,1 +1,7 @@
-document.getElementById('navbar').style.display = none;
+document.onscroll = function () {
+	if(window.pageYOffset > 1) {
+		document.getElementById('navbar').style.display = "inline";
+	} else {
+		document.getElementById('navbar').style.display = "none";
+	}
+}
