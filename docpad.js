e18c9152c3853ca5310bc840c4efa1c48e598ff8
Dominique Hazael-Massieux <dom@w3.org>
2016-08-26 15:54:37 +0200

    Use docpad to manage static content generation

---
+++ docpad.js
@@ -0,0 +1,16 @@
+module.exports = {
+    templateData: {
+        site: {
+            'root': '/'
+        }
+    },
+    environments: {
+        'static': {
+            templateData: {
+                site: {
+                    'root': '/developers/'
+                }
+            }
+        }
+    }
+}
